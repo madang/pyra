51e5b647460108f4b27a98990ae9fd6b96770782
madang <madang@biph.kiev.ua>
2013-02-23 00:29:26 +0200

    end of 2012-02-23: add parse.py and add todos into readme.md

--- README.md
+++ README.md
@@ -2,3 +2,14 @@ pyra
 ====
 
 News aggregator in python
+
+[Current todo]: read this http://docs.python-requests.org/en/latest/index.html (install and use Requests) 
+
+* use re to parse a result from 4vlada.net in parse.py
+
+
+* a plan for the politics topic
+	0) Read site list from a file
+	1) Do a google search with domain as this site
+	2) 
+	3) OUTPUT: A feed with results
--- test.py
+++ test.py
@@ -6,24 +6,25 @@ import pprint
 
 def showsome(searchfor,site):
   query_string='site:'+site+' '+searchfor
+  #~ query_string=searchfor
   query = urllib.urlencode({'q': query_string})
-  print '*** query ***'
-  print query
-  raw_input('anykey?')
+  #~ print '*** query ***'
+  #~ print query
+  #~ raw_input('anykey?')
   
   url = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&%s' % query
   search_response = urllib.urlopen(url)
-  print 'search response %s' % search_response
+  #~ print 'search response %s' % search_response
   search_results = search_response.read()
   
-  print '*** search_results ***'
-  pprint.PrettyPrinter(indent=4).pprint(search_results)
-  raw_input('anykey?')
+  #~ print '*** search_results ***'
+  #~ pprint.PrettyPrinter(indent=4).pprint(search_results)
+  #~ raw_input('anykey?')
   
   results = json.loads(search_results)
   data = results['responseData']
-  print "*** PRINTING DATA ***\n"
-#  pprint.PrettyPrinter(indent=4).pprint(data)
+  #~ print "*** PRINTING DATA ***\n"
+  #~ pprint.PrettyPrinter(indent=4).pprint(data)
 
   print 'Total results: %s' % data['cursor']['estimatedResultCount']
   hits = data['results']
@@ -32,6 +33,6 @@ def showsome(searchfor,site):
   print 'For more results, see %s' % data['cursor']['moreResultsUrl']
 
 
-site='vlada.net'
+site='4vlada.net'
 keyword='политолог'
 showsome(keyword,site)
