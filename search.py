d31e48d1f29445b7f5cc74ffe32ad07f3a49df24
madang <madang@biph.kiev.ua>
2013-03-17 12:36:34 +0200

    added posibilty to get results from the last day or week in search.py

--- search.py
+++ search.py
@@ -2,17 +2,25 @@
 # -*- coding: utf-8 -*-
 import requests
 import re
+from sys import exit
+
 
 # OK, seem's like we've got a way to search without dealing with anally fenced off corporations
-def startpage_parse(site,keyword):
+def startpage_parse(site,keyword,timeframe):
+	if  timeframe not in ['d','w']:
+		exit(1,'timeframe can be either "d" of "w", you supplied %s' % timeframe)
+		
 	engine_url = 'https://startpage.com/do/search'
-	q_string = 'host:' + site + ' ' + keyword
-	payload = {'q' : q_string}
+	#~ q_string = 'host:' + site + ' ' + keyword
+	payload = {'cat':'web',
+		'atleast_one' : keyword,
+		'with_domain' : site, 
+		'with_date': timeframe}
 	r = requests.get(engine_url, params = payload)
-
+	print r.url
 	# Uncomment lines below to save the html
-	#~ with open('res.html','w+') as out_file:
-		#~ out_file.write(r.text.encode('utf-8'))
+	with open('res.html','wb+') as out_file:
+		out_file.write(r.text.encode('utf-8'))
 		
 	links_list = re.findall(ur"<h3><a href=\'(.+)\' id",r.text)
 	print '\n\n'.join(links_list)
@@ -21,6 +29,7 @@ def startpage_parse(site,keyword):
 
 cache_filename = 'cache.pk'
 site = '4vlada.net'
-keyword = 'политолог'
+keyword = u'политолог'
+timeframe = 'w' # 'w' for last week, 'd' for yesterday
 
-startpage_parse(site,keyword)
+startpage_parse(site,keyword,timeframe)
