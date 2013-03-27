a0d45b8cc8c546f356bacd32eaa0e091f9b34c5d
madang <madang@biph.kiev.ua>
2013-03-27 13:14:54 +0200

    add a "parser" function to parse py, that chooses the correct parser besed on url

--- parse.py
+++ parse.py
@@ -5,6 +5,17 @@ import re
 import requests
 import search
 
+def parser(article_url):
+	"""Pick a parser depending on the domain"""
+	d = {u'4vlada.net':parse_4vlada_net}
+	
+	m = re.findall(ur'http://(.+?)/',article_url)
+	print '*'*24
+	print m
+	
+	key  = m[0]
+	return d[key](article_url)
+
 def parse_4vlada_net(article_url):
 	"""parse an article from 4vlada.net v0.0 17-03-2013"""
 	print "Article url: %s \n" % article_url
@@ -20,7 +31,8 @@ def parse_4vlada_net(article_url):
 		
 	# replace &quot; with "
 	article_string = article_string.replace(u'&quot;',u'"')
-		
+	print '='*40
+	print article_string.encode('utf-8')
 	return article_string.encode('utf-8')
 	# save results for analysis
 	#~ t_filename = 'res.txt'
@@ -37,6 +49,6 @@ print link_list
 
 parsed_article = []
 for article in link_list:
-	parsed_article.append(parse_4vlada_net(article))
+	parsed_article.append(parser(article))
 	
 print '\n\n'.join(parsed_article)
